f448f0354c8cc668b9b0d302ff85dc1cd2aa5937
Abdulsathar27 <abdulsatharnechithodi@gmail.com>
2026-05-13 10:08:46 +0530

    feat: create centralized design system constants

---
+++ lib/core/constant/app_colors.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color scaffoldBackground =
+      Color(0xFFF5F5F5);
+
+  static const Color primaryBlue =
+      Color(0xFF1976D2);
+
+  static const Color black =
+      Color(0xFF000000);
+
+  static const Color white =
+      Color(0xFFFFFFFF);
+
+  static const Color textDark =
+      Color(0xFF2C2C2C);
+
+  static const Color hintGrey =
+      Color(0xFF9E9E9E);
+
+  static const Color borderGrey =
+      Color(0xFFD9D9D9);
+
+  static const Color cardShadow =
+      Color(0x1A000000);
+
+  static const Color red =
+      Color(0xFFE53935);
+
+  static const Color green =
+      Color(0xFF43A047);
+}
---
+++ lib/core/constant/app_spacing.dart
@@ -0,0 +1,23 @@
+class AppSpacing {
+  static const double screenPadding = 20;
+
+  static const double cardRadius = 24;
+
+  static const double inputRadius = 20;
+
+  static const double buttonRadius = 16;
+
+  static const double cardPadding = 16;
+
+  static const double sectionSpacing = 24;
+
+  static const double itemSpacing = 16;
+
+  static const double smallSpacing = 8;
+
+  static const double mediumSpacing = 12;
+
+  static const double largeSpacing = 20;
+
+  static const double extraLargeSpacing = 32;
+}
