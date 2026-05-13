48658759d8a6d9254e0cd67112b6e3051cfe7559
Abdulsathar27 <abdulsatharnechithodi@gmail.com>
2026-05-13 18:28:43 +0530

    chore: setup environment configuration using flutter_dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:totalxtask/controller/auth_controller.dart';
 import 'package:totalxtask/controller/splash_controller.dart';
@@ -8,12 +9,11 @@ import 'package:totalxtask/core/theme/app_theme.dart';
 import 'package:totalxtask/views/auth/splash_screen.dart';
 import 'firebase_options.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
